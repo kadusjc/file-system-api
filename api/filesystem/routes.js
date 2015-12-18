88b75081eae61429e86e51430a241f12e3b83e97
Carlos Eduardo Ferreiro Correa <kadusjc@yahoo.com.br>
2015-12-18 16:33:26 -0200

    Implementing API DOC

--- api/filesystem/routes.js
+++ api/filesystem/routes.js
@@ -18,12 +18,30 @@ function options(fileId, req) {
 }
 
 router.
+
+
+  /**
+   * @api {post} /v1/file Save new File into FileSystem returning its id
+   * @apiGroup File
+   * @apiSuccess {String} name File name
+   * @apiSuccess {String} type contentType
+   * @apiSuccess {String} user User correlated with this file (file creator for example)
+   * @apiExample {json} Example usage:
+   *    curl -X POST http://file-system-api/v1/file \
+   *    -d 'name=Text.txt' \
+   *    -d 'type=text/plain' \
+   *    -d 'user=Kadu' \
+   * @apiSuccess {json} Success
+        HTTP/1.1 200 OK {id: 5674481b8796b87420683dd3 }
+   * @apiErrorExample {json} Error
+   *    HTTP/1.1 422 Unprocessable Entity
+   */
   post('/', (req, res) => {
 
     let fileId = new mongoose.Types.ObjectId()
     let writestream = gfs.createWriteStream(options(fileId, req))
     writestream.on('close', () => {
-      return res.status(200).json({fileId: fileId.toString()})
+      return res.status(200).json({id: fileId.toString()})
     });
     writestream.on('error', (err) => {
       return res.status(500).json({err: err})
@@ -32,6 +50,17 @@ router.
 
   }).
 
+  /**
+   * @api {get} /v1/file Retrieves a saved file inside file system by its id
+   * @apiGroup File
+   * @apiSuccess {String} id File id
+   * @apiExample {json} Example usage:
+   *    curl -X POST http://file-system-api/v1/file \
+   *    -d 'id=5674481b8796b87420683dd3' \   *
+   * @apiSuccess {Object} File to download
+   * @apiErrorExample {json} Error
+   *    HTTP/1.1 422 Unprocessable Entity
+   */
   get('/', (req, res) => {
 
     let fileId = mongoose.Types.ObjectId(req.query.id)
@@ -41,6 +70,20 @@ router.
     })
   }).
 
+  /**
+   * @api {delete} /v1/file Remove a saved file inside file system by its id
+   * @apiGroup File
+   * @apiSuccess {String} id File id
+   * @apiExample {json} Example usage:
+   *    curl -X DELETE http://file-system-api/v1/file/5674481b8796b87420683dd3
+   * @apiSuccessExample {json} Success-Response:
+   *    HTTP/1.1 200 OK
+   *    {
+   *      "message": "File 5674481b8796b87420683dd3 deleted successfully"
+   *    }
+   * @apiErrorExample {json} Error
+   *    HTTP/1.1 422 Unprocessable Entity
+   */
   delete('/:id', (req, res) => {
 
     var options = { _id: mongoose.Types.ObjectId(req.params.id) }
